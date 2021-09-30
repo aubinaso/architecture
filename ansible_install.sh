612d752b6af1ef073d1669c171270f4dc4bd9fe0
aubinaso <root@debian9.linuxvmimages.local>
2021-09-30 11:23:47 -0500

    ce qui a ete modifie est l'ajout de la partie installation de terraform dans deploy

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+
+# installer Ansible
+
 sudo apt update
 sudo apt upgrade
-sudo apt install python3-pip
-sudo pip3 install ansible==2.10
+sudo apt install -y python3-pip
+sudo pip3 install -y ansible==2.10
+
+# installer Terraform
+
+sudo apt update
+sudo apt upgrade
+sudo apt install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt install terraform
+
