f6403a0238dfd72eedd6d7d58ff7ef62fcbf0fa1
aubinaso <root@debian9.linuxvmimages.local>
2021-10-17 16:57:26 -0500

    mise a jour de la partie d'installation du ssh

--- ansible_install.sh
+++ ansible_install.sh
@@ -1,19 +1,43 @@
 #!/bin/bash
 
+sudo apt install -y sshpass
+
+sudo su - vagrant bash -c "ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/id_rsa -q -N ''"
+sudo su - vagrant bash -c "eval `ssh-agent`"
+sudo su - vagrant bash -c "ssh-add" 
+for srv in $(cat /etc/hosts | grep auto | awk '{print $2}');do
+	sudo su - vagrant bash -c "sshpass -p 'vagrant' ssh-copy-id -o StrictHostKeyChecking=no vagrant@$srv'
+done
 
 # installer Ansible
 
-sudo apt update
-sudo apt upgrade
-sudo apt install -y python3-pip
-sudo pip3 install -y ansible==2.10
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y python3-pip
+sudo bash -c "pip3 install -y ansible==2.10"
 
 # installer Terraform
 
-sudo apt update
-sudo apt upgrade
-sudo apt install -y gnupg software-properties-common curl
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt install terraform
+sudo apt-get update && sudo apt-get install terraform
+
+# installer Ansible
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y python3-pip
+sudo bash -c "pip3 install -y ansible==2.10"
+
+# installer Terraform
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get update && sudo apt-get install terraform
 
